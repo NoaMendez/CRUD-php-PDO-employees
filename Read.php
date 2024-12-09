588965da880dcaf78eb3f3403a95f664700e629b
NoaMendez <foxygamersfn@gmail.com>
2024-12-09 03:43:31 -0500

    index

--- Read.php
+++ Read.php
@@ -79,7 +79,7 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
                         <label>Salary</label>
                         <p><b><?php echo $row["salary"]; ?></b></p>
                     </div>
-                    <p><a href="index.php" class="btn btn-primary">Back</a></p>
+                    <p><a href="Index.php" class="btn btn-primary">Back</a></p>
                 </div>
             </div>        
         </div>
